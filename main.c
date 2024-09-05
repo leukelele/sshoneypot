d59bf9768d03f8027bc63a1a23feecdead5b758b
leukelele <70491331+leukelele@users.noreply.github.com>
2024-09-04 21:00:51 -0500

    init use with libssh

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
-#include <stdlib.h>
+#include <stdio.h>            // used for:
+                              // - printf
 
-#include <libssh/server.h>
+#include <libssh/server.h>    // used for:
+                              // - ssh_bind, the data type for ssh server bind
+                              // - ssh_bind_options_set, options for ssh server binding
 
-#define DEFAULT_PORT 22
 
 int main(int argc, char **argv) {
+  int port = 22;
+  ssh_bind sshbind = ssh_bind_new();
+  
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDADDR, "localhost"); 
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT, &port); 
+
+  if (ssh_bind_listen(sshbind) < 0) {
+    printf("Error listening to socket: %s\n", ssh_get_error(sshbind));
+    return -1;
+  }
+
   return 0;
 }
