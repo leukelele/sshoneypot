c360c134ed19909d5e891409ac7a908a0eef1739
leukelele <70491331+leukelele@users.noreply.github.com>
2024-09-09 13:08:47 -0500

    macros to replace variables

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
-#include <stdio.h>            // used for:
-                              // - printf
-
-#include <libssh/server.h>    // used for:
-                              // - ssh_bind, the data type for ssh server bind
-                              // - ssh_bind_options_set, options for ssh server binding
+#include "config.h"           /* defined macros:
+			       * - addr bound to
+			       * - default port
+			       * - keyfile directory
+			       */
+#include <stdio.h>            /* used for:
+                               * - printf
+			       */
+#include <libssh/server.h>    /* used for:
+                               * - ssh_bind, the data type for ssh server bind
+                               * - ssh_bind_options_set, options for ssh server binding
+			       */
 
 
 int main(int argc, char **argv) {
-  int port = 22;
+
   ssh_bind sshbind = ssh_bind_new();
-  
-  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDADDR, "localhost"); 
-  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT, &port); 
-  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, "/etc/ssh/ssh_host_rsa_key");
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDADDR, LISTENADDR); 
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT, DEFLT_PORT); 
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, KEYFILE_DIR);
 
   if (ssh_bind_listen(sshbind) < 0) {
     printf("Error listening to socket: %s\n", ssh_get_error(sshbind));
