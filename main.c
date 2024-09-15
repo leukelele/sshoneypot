1370409856f2659236cb3dccf627a3760442eecf
leukelele <70491331+leukelele@users.noreply.github.com>
2024-09-15 18:26:31 -0500

    included missing library

--- main.c
+++ main.c
@@ -12,10 +12,14 @@
                                * - ssh_bind, the data type for ssh server bind
                                * - ssh_bind_options_set, options for ssh server binding
 			                         */
+#include <libssh/libssh.h>    /* used for:
+			                         */
 
 int main(int argc, char **argv) {
 
+        connection = 
   ssh_bind sshbind = ssh_bind_new();
+
   ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDADDR, LISTENADDR); 
   ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT, DEFLT_PORT); 
   ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, KEYFILE_DIR);
